293a88674dc5b12ecd2290372db1d12ce12273d5
taoya7 <569781231@qq.com>
2024-06-19 14:52:12 +0800

    feat: Dockerfile

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/**/*.ts'], // 入口文件配置，此配置将包含 src 目录下所有的 .ts 文件
+  entry: ['src/**/*.ts', '!**/*.test.ts'],
   format: ['esm'], // 输出格式配置，这里设置为 ESM（ECMAScript 模块）
   dts: false, // 是否生成 TypeScript 声明文件 (.d.ts)，这里设置为不生成
   splitting: false, // 是否启用代码拆分，用于代码分割，这里禁用
